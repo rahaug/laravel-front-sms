e40c9fef1ddb4a96467db7ad46666da0dd126bd0
Rolf Haug <rolf@rah.no>
2020-05-29 09:02:22 +0200

    feat: add example of dynamic message with multiple dependencies

---
+++ tests/Messages/MultiDependencyMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Messages;
+
+use RolfHaug\FrontSms\Notifications\SmsNotification;
+use Tests\Article;
+
+class MultiDependencyMessage extends SmsNotification
+{
+    public $message = 'Hi %s, you have a new comment on your "%s" article. Read it here: %s';
+    /**
+     * @var Article
+     */
+    private $article;
+
+    public function __construct($message = null, Article $article)
+    {
+        $this->article = $article;
+        if ($message) {
+            $this->message = $message;
+        }
+        parent::__construct($this->message);
+    }
+
+    public function getMessage($notifiable)
+    {
+        return vsprintf($this->message, [
+            $notifiable->name,
+            $this->article->title,
+            $this->article->link
+        ]);
+    }
+}
