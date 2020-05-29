6fd713c6e95b37bf4f56d9e263eec559cbc2e25d
Rolf Haug <rolf@rah.no>
2020-05-29 07:13:34 +0200

    fix: typos in migraitions

--- src/FrontSmsServiceProvider.php
+++ src/FrontSmsServiceProvider.php
@@ -4,6 +4,7 @@ namespace RolfHaug\FrontSms;
 
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
+use RolfHaug\FrontSms\Console\Commands\MakeSmsCommand;
 
 class FrontSmsServiceProvider extends ServiceProvider
 {
@@ -11,10 +12,23 @@ class FrontSmsServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
+        $this->mergeConfigFrom(__DIR__.'/../config/front-sms.php', 'front-sms');
+
         if ($this->app->runningInConsole()) {
-            if (! class_exists('CreateSmsStatusesTable')) {
+            $this->commands([
+                MakeSmsCommand::class
+            ]);
+
+            $this->publishes([
+                __DIR__.'/../config/front-sms.php' => config_path('front-sms.php')
+            ], 'config');
+
+            if (! class_exists('CreateFrontMessagesStatusTable,')) {
                 $this->publishes([
-                    __DIR__.'/../database/migrations/create_front_message_statuses_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_sms_statuses_table.php'),
+                    __DIR__.'/../database/migrations/create_front_messages_table.php.stub' => database_path('migrations/'.date('Y_m_d_His_', time()).'create_front_messages_table.php'),
+                    __DIR__.'/../database/migrations/create_front_message_statuses_table.php.stub' => database_path('migrations/'.date('Y_m_d_His_', time()).'create_front_message_statuses_table.php'),
+                    __DIR__.'/../database/migrations/add_phone_column_to_user_table.php.stub' => database_path('migrations/'.date('Y_m_d_His_', time()).'add_phone_column_to_user_table.php'),
+                    __DIR__.'/../database/migrations/add_country_code_column_to_user.php.stub' => database_path('migrations/'.date('Y_m_d_His_', time()).'add_country_code_column_to_user.php'),
                 ], 'migrations');
             }
         }
@@ -22,8 +36,6 @@ class FrontSmsServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/front-sms.php', 'front-sms');
-
         $this->app->when(FrontClient::class)->needs(Client::class)->give(function () {
             return new Client([
                 'headers' => [
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,7 +17,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $this->withFactories(__DIR__.'/../database/factories');
 
         // Add phone to auth model
-        include_once __DIR__.'/../database/migrations/add_phone_column_to_user.php.stub';
+        include_once __DIR__.'/../database/migrations/add_phone_column_to_user_table.php.stub';
         include_once __DIR__.'/../database/migrations/add_country_code_column_to_user.php.stub';
         (new \AddPhoneColumnToUserTable)->up();
         (new \AddCountryCodeColumnToUser)->up();
@@ -40,7 +40,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
             'prefix' => '',
         ]);
         $app['config']->set('app.debug', true);
-        $app['config']->set('auth.model', User::class);
+        $app['config']->set('auth.providers.users.model', User::class);
         $app->bind(ExceptionHandler::class, ExceptionLogger::class);
 
         // import the CreatePostsTable class from the migration
@@ -48,8 +48,8 @@ class TestCase extends \Orchestra\Testbench\TestCase
         include_once __DIR__.'/../database/migrations/create_front_message_statuses_table.php.stub';
 
         // run the up() method of that migration class
-        (new \CreateFrontMessagesStatusTable)->up();
         (new \CreateFrontMessagesTable)->up();
+        (new \CreateFrontMessageStatusesTable)->up();
     }
 
     /**
